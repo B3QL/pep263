e451cdb4ae5ff42d1cfc4cee37dcbada912810cd
Bartłomiej Kurzeja <bartlomiej@kurzeja.it>
2016-10-29 20:56:54 +0200

    Refactor check_file_encoding to check_encoding

--- tests/test_check_encoding.py
+++ tests/test_check_encoding.py
@@ -1,27 +1,64 @@
 import io
 import pytest
-from pep263 import check_file, Encoding
+from pep263.core import check_encoding, _check_file_encoding, EncodingInfo
 
 
-def test_file_with_no_content():
+def test_file_without_permission(caplog, tmpdir):
+    test_dir = tmpdir.mkdir('test')
+    test_file = test_dir.ensure('test.py')
+    test_file.chmod(000)
+    check_encoding(test_file.strpath)
+    assert len(caplog.records) == 1
+    assert 'Cannot open file' in caplog.text
+    assert test_file.strpath in caplog.text
+
+
+def test_file_not_found(caplog):
+    filename = 'not_existing_file.py'
+    check_encoding(filename)
+    assert len(caplog.records) == 1
+    assert 'File not found' in caplog.text
+    assert filename in caplog.text
+
+
+def test_file_without_content(caplog, tmpdir):
+    test_dir = tmpdir.mkdir('test')
+    test_file = test_dir.ensure('test.py')
+    check_encoding(test_file.strpath)
+    assert len(caplog.records) == 1
+    assert 'Encoding not found' in caplog.text
+    assert test_file.strpath in caplog.text
+
+
+def test_file_with_invalid_coding_2(caplog, tmpdir):
+    test_dir = tmpdir.mkdir('test')
+    test_file = test_dir.join('test.py')
+    test_file.write('# -*- coding: utf-42 -*-')
+    check_encoding(test_file.strpath)
+    assert len(caplog.records) == 1
+    assert 'Unknown encoding' in caplog.text
+    assert test_file.strpath in caplog.text
+
+
+def test_file_without_content_2():
     f = io.StringIO()
     with pytest.raises(LookupError) as excinfo:
-        check_file(f)
+        _check_file_encoding(f)
     assert str(excinfo.value) == 'encoding not found'
 
 
 def test_file_with_utf8_coding():
     f = io.StringIO('# -*- coding: utf-8 -*-')
-    encoding = check_file(f)
-    assert isinstance(encoding, Encoding)
+    encoding = _check_file_encoding(f)
+    assert isinstance(encoding, EncodingInfo)
     assert encoding.name == 'utf-8'
     assert encoding.lineno == 1
 
 
 def test_file_with_utf16_coding():
     f = io.StringIO('# -*- coding: utf-16 -*-')
-    encoding = check_file(f)
-    assert isinstance(encoding, Encoding)
+    encoding = _check_file_encoding(f)
+    assert isinstance(encoding, EncodingInfo)
     assert encoding.name == 'utf-16'
     assert encoding.lineno == 1
 
@@ -31,16 +68,16 @@ def test_file_with_utf8_coding_in_second_line():
                     '# -*- coding: utf-8 -*-']
 
     f = io.StringIO('\n'.join(file_content))
-    encoding = check_file(f)
-    assert isinstance(encoding, Encoding)
+    encoding = _check_file_encoding(f)
+    assert isinstance(encoding, EncodingInfo)
     assert encoding.name == 'utf-8'
     assert encoding.lineno == 2
 
 
 def test_file_with_utf8_plain_text_coding():
     f = io.StringIO('# This Python file uses the following encoding: utf-8')
-    encoding = check_file(f)
-    assert isinstance(encoding, Encoding)
+    encoding = _check_file_encoding(f)
+    assert isinstance(encoding, EncodingInfo)
     assert encoding.name == 'utf-8'
     assert encoding.lineno == 1
 
@@ -50,8 +87,8 @@ def test_file_with_utf8_editor_coding():
                     '# coding: latin-1']
 
     f = io.StringIO('\n'.join(file_content))
-    encoding = check_file(f)
-    assert isinstance(encoding, Encoding)
+    encoding = _check_file_encoding(f)
+    assert isinstance(encoding, EncodingInfo)
     assert encoding.name == 'latin-1'
     assert encoding.lineno == 2
 
@@ -59,7 +96,7 @@ def test_file_with_utf8_editor_coding():
 def test_file_with_missing_coding_prefix():
     f = io.StringIO('# utf-8')
     with pytest.raises(LookupError) as excinfo:
-        check_file(f)
+        _check_file_encoding(f)
     assert str(excinfo.value) == 'encoding not found'
 
 
@@ -70,12 +107,12 @@ def test_file_with_utf8_coding_in_third_line():
 
     f = io.StringIO('\n'.join(file_content))
     with pytest.raises(LookupError) as excinfo:
-        check_file(f)
+        _check_file_encoding(f)
     assert str(excinfo.value) == 'encoding not found'
 
 
 def test_file_with_invalid_coding():
     f = io.StringIO('# -*- coding: utf-42 -*-')
     with pytest.raises(ValueError) as excinfo:
-        check_file(f)
+        _check_file_encoding(f)
     assert str(excinfo.value) == 'unknown encoding: utf-42'
