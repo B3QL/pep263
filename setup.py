05d31b24abe273412d0df071f7953f1b67e08f2b
Bartłomiej Kurzeja <b.kurzeja@clearcode.cc>
2018-06-01 11:18:06 +0200

    Remove tox

--- setup.py
+++ setup.py
@@ -12,12 +12,18 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
+
+test_requirements = [
+    'pytest==3.6.0',
+    'pylama==7.4.3',
+]
+
 setup(
     name='pep263',
     version=find_version('pep263', '__init__.py'),
     license='MIT',
-    packages=find_packages(exclude=['docs', 'tests*']),
-    tests_require=['tox'],
+    packages=find_packages(exclude=['docs', 'tests']),
+    tests_require=test_requirements,
     classifiers=[
         'License :: OSI Approved :: MIT License',
     ],
