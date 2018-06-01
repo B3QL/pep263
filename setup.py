c91d225fbca098590c7b3eba44c15b55ee78f99e
Bartłomiej Kurzeja <b.kurzeja@clearcode.cc>
2018-06-01 15:59:03 +0200

    Run tests from setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 """Package configuration."""
 import os
 import re
+import sys
 
 from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
 
 
 def find_version(*file_paths):
@@ -14,6 +16,24 @@ def find_version(*file_paths):
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
+
+
+class PyTest(TestCommand):
+    """Pytest tests runner."""
+
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def __init__(self, *args, **kwargs):
+        """Initalize."""
+        super(PyTest, self).__init__(*args, **kwargs)
+        self.pytest_args = ''
+
+    def run_tests(self):
+        """Run tests."""
+        import shlex
+        import pytest  # import here, cause outside the eggs aren't loaded
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
 
 
 test_requirements = [
@@ -29,6 +49,7 @@ setup(
     license='MIT',
     packages=find_packages(exclude=['docs', 'tests']),
     tests_require=test_requirements,
+    cmdclass={'test': PyTest},
     classifiers=[
         'License :: OSI Approved :: MIT License',
     ],
